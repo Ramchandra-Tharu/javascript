b2f2a39044bb4617f2d2053c5c157ec460f0401b
Ramchandra Tharu <113186711+Ramchandra-Tharu@users.noreply.github.com>
2025-09-20 15:22:39 +0000

    datatype is created

---
+++ Basic/datatype.js
@@ -0,0 +1,21 @@
+//Data type in javascript.
+/*
+Number = for integer
+bigInit = for integer with high level numbers
+String
+Boolean 
+null 
+undefine
+symbol
+*/
+// Example
+
+const Number = 23
+let String = "sirzan "
+let isActive = true;
+const Current = null;
+
+console.log(Number)
+console.log(String)
+console.log(isActive)
+console.log(Current)
