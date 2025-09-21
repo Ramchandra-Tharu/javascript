fd392eefecfb75ada7339310a847771b8dc11387
Ramchandra Tharu <113186711+Ramchandra-Tharu@users.noreply.github.com>
2025-09-21 17:33:49 +0000

    array is created

---
+++ 2_Basic/array.js
@@ -0,0 +1,24 @@
+
+// creating array
+
+let arr1 = [2,4,6,8]
+console.log(arr1)
+let arr2 = new Array(2,4,3,2,6,4,8)
+console.log(arr2)
+ 
+let arr3 = [];
+console.log(arr3)
+
+//accessing array elements
+
+let students = ["Ram","sirzan", "Raman", "ravina", "saur"]
+console.log(students[1]) //can access the element by using index
+
+// add element or remove
+
+arr1.push(10) // add at end
+arr1.pop(10) // remove at the end
+arr1.unshift(0) // add at first 
+arr1.shift(0) // remove at the first
+
+console.log(arr1)
