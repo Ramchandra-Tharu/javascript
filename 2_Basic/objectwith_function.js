0030b01c3727bce52f97800c5463c8699d16fc1e
Ramchandra Tharu <113186711+Ramchandra-Tharu@users.noreply.github.com>
2025-09-25 18:02:14 +0000

    this file is object with function

---
+++ 2_Basic/objectwith_function.js
@@ -0,0 +1,9 @@
+const student = {
+    name : "Ramchandra",
+    age : 21,
+    Course : "computer application"
+}
+function objectFunction (anyobject){
+    console.log (`your name is ${anyobject.name}`)
+}
+objectFunction(student) 
