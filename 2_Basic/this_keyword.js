8bee99335bbad70a4b5101882ce707d293e04ce3
Ramchandra Tharu <113186711+Ramchandra-Tharu@users.noreply.github.com>
2025-09-27 10:26:09 +0000

    this keyword

---
+++ 2_Basic/this_keyword.js
@@ -0,0 +1,14 @@
+const footballPlyer = {
+    name : "Ramchandra Tharu",
+    dob : "24-09-2003",
+    playerPosition : "Left winger, center forward",
+    
+    res : function () {
+        console.log("player name is " + this.name)
+        console.log("player date  of birth  is " + this.dob)
+        console.log("player position is " + this.playerPosition)
+
+    },
+
+}
+    footballPlyer.res ();
